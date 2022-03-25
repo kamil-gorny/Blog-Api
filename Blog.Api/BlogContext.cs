b5c5fd3aed94f084db4274297f7eada2cfa7a7c0
kamil-gorny <kamil.gorny99@gmail.com>
2022-03-25 23:24:52 +0100

    feat: Add DbContext

---
+++ Blog.Api/BlogContext.cs
@@ -0,0 +1,16 @@
+using Blog_Api.DataModel.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Api;
+
+public class BlogContext : DbContext
+{
+    public BlogContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User>? Users { get; set; }
+    
+    public DbSet<Post> Post { get; set; }
+}
