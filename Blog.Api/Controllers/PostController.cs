46d67f99c568eb6a1077d0edabb191a6dd0ec9b6
kamil-gorny <kamil.gorny99@gmail.com>
2022-03-26 15:45:02 +0100

    feat: Add post controller

--- Blog.Api/Controllers/PostController.cs
+++ Blog.Api/Controllers/PostController.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Blog_Api;
 using Blog_Api.DataModel.Entities;
 using Blog_Api.Exceptions;
 using Blog_Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Blog_Api.Controllers
 {
@@ -23,13 +17,14 @@ namespace Blog_Api.Controllers
             _postService = postService;
         }
 
-        // GET: api/Post/5
-        [HttpGet("{id}")]
+        [HttpGet("{id:guid}")]
+        [AllowAnonymous]
         public async Task<ActionResult<Post>> GetPost(Guid id)
         {
             try
             {
-                await _postService.ReadPost(id);
+                var result = await _postService.ReadPost(id);
+                return Ok(result);
             }
             catch (NotFoundException ex)
             {
@@ -39,80 +34,73 @@ namespace Blog_Api.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            
-            
-            
-            var post = await _context.Post.FindAsync(id);
-
-            if (post == null)
-            {
-                return NotFound();
-            }
-
-            return post;
         }
 
-        // PUT: api/Post/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPost(Guid id, Post post)
+
+        [HttpGet]
+        [AllowAnonymous]
+
+        public async Task<ActionResult<List<Post>>> GetPosts()
         {
-            if (id != post.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(post).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
+                var result = await _postService.ReadAllPosts();
+                return Ok(result);
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception ex)
             {
-                if (!PostExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
-
-            return NoContent();
         }
-
-        // POST: api/Post
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Post>> PostPost(Post post)
+        
+        
+        [HttpPut]
+        public async Task<IActionResult> PutPost(Post post)
         {
-            _context.Post.Add(post);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPost", new { id = post.Id }, post);
-        }
-
-        // DELETE: api/Post/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePost(Guid id)
-        {
-            var post = await _context.Post.FindAsync(id);
-            if (post == null)
+            try
+            {
+                await _postService.UpdatePost(post);
+                return Ok();
+            }
+            catch (NotFoundException)
             {
                 return NotFound();
             }
-
-            _context.Post.Remove(post);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
-
-        private bool PostExists(Guid id)
+        
+        [HttpPost]
+        public async Task<ActionResult> PostPost(Post post)
         {
-            return _context.Post.Any(e => e.Id == id);
+            try
+            {
+                await _postService.CreatePost(post);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            
         }
+        
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeletePost(Guid id)
+        {
+            try
+            {
+                await _postService.DeletePost(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            
+        }
+        
     }
 }
